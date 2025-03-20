36fd37044e84a655cb6151cda5578f21c9af5b37
isma <i00934562@gmail.com>
2025-03-20 20:22:39 -0300

    Se creo el modal

--- src/components/ModalLogin.jsx
+++ src/components/ModalLogin.jsx
@@ -1,3 +1,6 @@
+import "../components/css/login.css"
+import icoface from "/img/facebook-icon.svg"
+import icongoogle from "/img/google-icon.svg"
 export const ModalLogin=()=>{
     return(
         <>
@@ -9,15 +12,48 @@ export const ModalLogin=()=>{
   <div className="modal-dialog">
     <div className="modal-content">
       <div className="modal-header">
-        <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <h1 className="modal-title " id="exampleModalLabel">Logearse </h1>
         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div className="modal-body">
-       <h1>MODAL LOGIN</h1>
+        <form action="">
+            <label htmlFor="">Correo Electronico</label><br />
+            <input type="text" /><br />
+            <label htmlFor="">Contraseña</label>
+            <br />
+            <input type="number" />
+        </form>
+
+      <button
+                  type="button"
+                  className="logingoogle "
+                 
+                >
+                  <img
+                    src={icongoogle}
+                    alt="Google Icon"
+                    className="tamañoLogoGyF "
+                  />
+                  Iniciar con Google
+                </button>
+                <button
+                  type="button"
+                  className="loginface "
+                 
+                >
+                  <img
+                    src={icoface}
+                    alt="Google Icon"
+                    className="tamañoLogoGyF me-2"
+                  />
+                  Iniciar con Google
+                </button>
+               
+              
       </div>
       <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Save changes</button>
+        <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
+        <button type="button" className="btn btn-success">Iniciar</button>
       </div>
     </div>
   </div>
